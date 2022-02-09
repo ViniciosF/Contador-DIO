b62648552ce0074950d10a26ed56b82af3ae7b3a
ViniciosF <vinicios.thaynara@hotmail.com>
2022-02-09 16:46:54 -0300

    adicionando comentários

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,22 @@ function decrement(){
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
 }
+
+
+//let count = 0;
+
+//const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+//function increment() {
+//	count++;
+//	CURRENT_NUMBER.innerHTML = count;
+//}
+
+//function decrement() {
+//	count--;
+//	CURRENT_NUMBER.innerHTML = count;
+//}
+
+//function test() {
+//	kdowkdpo;
+//}
